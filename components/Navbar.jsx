6f176ed404d59e50560cbf4cfb861316c083dd3b
vijaykumar <vijaykumar@vijaykumars-MacBook-Air.local>
2024-03-22 16:13:16 +0530

    nav-bottom styles added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,23 @@
+import Image from "next/image";
 import styles from "@/components/Navbar.module.scss";
+
 export default function Navbar() {
-  return <div className={styles.navbar}>Navbar</div>;
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.navTop}></div>
+      <div className={styles.navBottom}>
+        <div>
+          <div>
+            <Image src="/images/ham.png" width={17} height={14} />
+          </div>
+          <div>ALL</div>
+          <div>Today's Deals</div>
+          <div>Customer Service</div>
+          <div>Registry</div>
+          <div>Gift Cards</div>
+          <div>Sell</div>
+        </div>
+      </div>
+    </div>
+  );
 }
