80cca523288734a65c7bc31979965977e6604ba3
vijaykumar <vijaykumar@vijaykumars-MacBook-Air.local>
2024-03-22 18:12:02 +0530

    media queries added

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,4 +1,10 @@
-import styles from "@/components/Banner.module.scss";
+// import styles from "@/components/Banner.module.scss";
+import Image from "next/image";
 export default function Banner() {
-  return <div className={styles.banner}></div>;
+  return (
+    <div>
+      <Image src="/images/banner.png" alt="My Icon" width={1500} height={600} />
+    </div>
+  );
+  // return <div className={styles.banner}></div>;
 }
