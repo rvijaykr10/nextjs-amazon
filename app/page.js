69e17d0540378a5d1187c7ba915056df3bada500
vijaykumar <vijaykumar@vijaykumars-MacBook-Air.local>
2024-03-22 17:31:48 +0530

    banner added

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
+import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
 
 export default function page() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Banner />
+    </>
+  );
 }
---
+++ components/Banner.jsx
@@ -0,0 +1,4 @@
+import styles from "@/components/Banner.module.scss";
+export default function Banner() {
+  return <div className={styles.banner}></div>;
+}
