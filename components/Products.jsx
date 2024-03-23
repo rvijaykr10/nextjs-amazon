a1f88f8f8608f7b0544e3977165c7438652c4c41
vijaykumar <vijaykumar@vijaykumars-MacBook-Air.local>
2024-03-23 09:04:43 +0530

    placeholder added for cards

--- components/Products.jsx
+++ components/Products.jsx
@@ -3,36 +3,40 @@ export default function Products() {
   return (
     <div className={styles.products}>
       <section className={styles.cardRow}>
-        <div>card 1</div>
-        <div>card 2</div>
-        <div>card 3</div>
-        <div>card 4</div>
+        <div className={styles.cardTypeOne}>
+          <div className={styles.heading}>heading</div>
+          <div className={styles.photos}>photos</div>
+          <div className={styles.action}>action</div>
+        </div>
+        <div className={styles.cardTypeOne}>card 2</div>
+        <div className={styles.cardTypeOne}>card 3</div>
+        <div className={styles.cardTypeOne}>card 4</div>
       </section>
       <section className={styles.cardRow}>
-        <div>card 5</div>
-        <div>card 6</div>
-        <div>card 7</div>
-        <div>card 8</div>
+        <div className={styles.cardTypeOne}>card 5</div>
+        <div className={styles.cardTypeOne}>card 6</div>
+        <div className={styles.cardTypeOne}>card 7</div>
+        <div className={styles.cardTypeOne}>card 8</div>
       </section>
       <section className={styles.row}>
         <div>row 1</div>
         <div>row 2</div>
       </section>
       <section className={styles.cardRow}>
-        <div>card 9</div>
-        <div>card 10</div>
-        <div>card 11</div>
-        <div>card 12</div>
+        <div className={styles.cardTypeOne}>card 9</div>
+        <div className={styles.cardTypeOne}>card 10</div>
+        <div className={styles.cardTypeOne}>card 11</div>
+        <div className={styles.cardTypeOne}>card 12</div>
       </section>
       <section className={styles.row}>
         <div>row 3</div>
         <div>row 4</div>
       </section>
       <section className={styles.cardRow}>
-        <div>card 13</div>
-        <div>card 14</div>
-        <div>card 15</div>
-        <div>card 16</div>
+        <div className={styles.cardTypeOne}>card 13</div>
+        <div className={styles.cardTypeOne}>card 14</div>
+        <div className={styles.cardTypeOne}>card 15</div>
+        <div className={styles.cardTypeOne}>card 16</div>
       </section>
       <section className={styles.row}>
         <div>row 5</div>
