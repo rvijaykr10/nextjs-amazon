5eb8d725712487e1cedb061dfa1f6735bf395134
vijaykumar <vijaykumar@vijaykumars-MacBook-Air.local>
2024-03-23 10:31:24 +0530

    banner products overlapping styles done

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,9 +1,10 @@
-// import styles from "@/components/Banner.module.scss";
+import styles from "@/components/Banner.module.scss";
 import Image from "next/image";
 export default function Banner() {
   return (
-    <div>
+    <div className={styles.banner}>
       <Image src="/images/banner.png" alt="My Icon" width={1500} height={600} />
+      <div className={styles.bannerOverlay} />
     </div>
   );
   // return <div className={styles.banner}></div>;
